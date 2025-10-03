23fdf0798675df7c95df99707feee665a299eebf
jade ! <150134796+kiitaristi@users.noreply.github.com>
2025-10-03 00:52:13 +0000

    HTML now updates with proper counter information

--- src/main.ts
+++ src/main.ts
@@ -22,5 +22,6 @@ const counterElement = document.getElementById("counter")!;
 button.addEventListener("click", () => {
   // This looks like to a good place to add some logic!
   counter += 1;
+  counterElement.innerHTML = counter.toString();
   console.log("I have these thingies:", button, counterElement, counter);
 });
